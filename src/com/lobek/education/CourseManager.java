23a89e9dbbaa4b50b7b9adae8741c6796dd2a236
Abeleencon <abiodunowoseje@gmail.com>
2018-02-03 00:54:18 -0500

    Updated the user commands for course management

--- src/com/lobek/education/CourseManager.java
+++ src/com/lobek/education/CourseManager.java
@@ -1,6 +1,7 @@
 package com.lobek.education;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -56,6 +57,29 @@ public class CourseManager implements DataTypeManager {
 					System.out.format("%n%n");
 					printInstructions();
 					break;
+				case 'a':
+					addNewCourse();
+					System.out.format("%n%n");
+					printInstructions();
+					break;
+				case 'd':
+					System.out.print("Please enter the course id to be deleted:\t ");
+					Scanner scn = new Scanner(System.in);
+					int id = scn.nextInt();
+					deleteCourse(id);
+					System.out.format("%n%n");
+					printInstructions();
+					break;
+				case 'm':
+				try {
+					modifyCourse();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+					System.out.format("%n%n");
+					printInstructions();
+					break;
 										
 				case 'b': // Go Back
 					break;
@@ -65,6 +89,7 @@ public class CourseManager implements DataTypeManager {
 					
 			}
 		} while (c != 'b');
+	console.close();
 	}
 	
 	private void printInstructions() {
@@ -82,7 +107,163 @@ public class CourseManager implements DataTypeManager {
 	}
 	
 	public void listAll() {
+				
+			try {
+			Statement stmt = this.conn.createStatement();
+			String descriptionOut = "";
+			ResultSet rs = stmt.executeQuery("SELECT * FROM course");
+			while (rs.next()) {
+				int id = rs.getInt("id");
+				String department = rs.getString("dept_id");
+				String courseCode = rs.getString("code");
+				String courseName = rs.getString("name");
+				String description = rs.getString("description");
+				if(description.length() > 20) {
+				   descriptionOut = description.substring(0,20) + "...";
+				}
+				float courseCredits = rs.getFloat("credits");
+				
+				System.out.format("\"%s\", \"%s\", \"%s\", \"%s\", \"%s\", \"%s\"\n", id, department, courseCode, courseName, descriptionOut, courseCredits);
+			}
+			stmt.close();
+			} catch (Exception e) {
+				System.err.println("Got an exception!");
+				System.err.println(e.getMessage());
+		}
+		
 		
 	}
-
+	public void addNewCourse() {
+		
+		try {
+			//Statement stmt = this.conn.createStatement();
+			Scanner scn = new Scanner(System.in);
+			System.out.print("Please enter the course details below:\n ");
+			System.out.print("Dept ID: ");
+			String dept_id = scn.nextLine();
+			System.out.print("Code: ");
+			String code = scn.nextLine();
+			System.out.print("Name: ");
+			String name = scn.nextLine();
+			System.out.print("Description: ");
+			String description = scn.nextLine();
+			System.out.print("Credits: ");
+			float credits = scn.nextFloat();
+			
+			scn.close();			
+			
+			String sql = ("INSERT INTO course(dept_id, code, name, description, credits) VALUES(?,?,?,?,?)");
+			PreparedStatement preparedStatement = this.conn.prepareStatement(sql);
+			preparedStatement.setString(1, dept_id);
+			preparedStatement.setString(2, code);
+			preparedStatement.setString(3, name);
+			preparedStatement.setString(4, description);
+			preparedStatement.setFloat(5, credits);
+			preparedStatement.executeUpdate();
+			
+			preparedStatement.close();
+			conn.close();
+			//stmt.close();
+			System.out.print("Thank you for adding a new course!\n ");
+			} catch (SQLException e) {
+            System.out.println(e.getMessage());
+			}
+            catch (Exception e) {
+				System.err.println("Got an exception!");
+				System.err.println(e.getMessage());
+		}
+		   
+        }
+		
+	public void deleteCourse(int id) {
+		
+		String sql = "DELETE FROM course WHERE id = ?";
+		
+		try {
+			PreparedStatement preparedStatement = this.conn.prepareStatement(sql);
+			preparedStatement.setInt(1, id);
+			preparedStatement.executeUpdate();
+		} catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+		System.out.print("The course has been deleted!\n ");
+	}
+	public void modifyCourse() throws SQLException {
+		
+		Scanner scn = new Scanner(System.in);
+		System.out.print("Please enter the course id to be modified:\n ");
+		int idModify = scn.nextInt();
+		try {
+			Statement stmt = this.conn.createStatement();
+		ResultSet rs = stmt.executeQuery("SELECT * FROM course WHERE id = ? " + idModify );
+		while (rs.next()) {
+			int id = rs.getInt("id");
+			String department = rs.getString("dept_id");
+			String courseCode = rs.getString("code");
+			String courseName = rs.getString("name");
+			String description = rs.getString("description");
+									
+			System.out.format("ID: %s\n, Department ID: %s\n, Code: %s\n, Name: %s\n, Description: %s\n", id, department, courseCode, courseName, description);
+		}
+		stmt.close();
+		} catch (Exception e) {
+			System.err.println("Got an exception!");
+			System.err.println(e.getMessage());
+	}
+		System.out.print("Enter a command below to edit this course:\n ");
+		System.out.println("d           Change course department id");
+		System.out.println("a           Change course code");
+		System.out.println("d           Change course name");
+		System.out.println("m           Change course description");
+		System.out.println("b           Go Back");
+		System.out.print("Please enter your command to continue: ");
+		
+		char u; 
+				
+		do {
+			String consoleLine = scn.nextLine();
+			u = consoleLine.charAt(0);
+			
+			switch(u) {
+				case 'd':
+					System.out.print("Dept ID: ");
+					String dept_id = scn.nextLine();
+					String sql1 = ("UPDATE course SET (dept_id) VALUES(?)");
+					PreparedStatement preparedStatement = this.conn.prepareStatement(sql1);
+					preparedStatement.setString(1, dept_id);
+					break;
+				case 'c':
+					System.out.print("Course Code: ");
+					String courseCode = scn.nextLine();
+					String sql2 = ("UPDATE course SET (code) VALUES(?)");
+					PreparedStatement preparedStatement2 = this.conn.prepareStatement(sql2);
+					preparedStatement2.setString(1, courseCode);
+					break;
+				case 'n':
+					System.out.print("Name: ");
+					String courseName = scn.nextLine();
+					String sql3 = ("UPDATE course SET (name) VALUES(?)");
+					PreparedStatement preparedStatement3 = this.conn.prepareStatement(sql3);
+					preparedStatement3.setString(1, courseName);
+					break;
+				case 't':
+					System.out.print("Description: ");
+					String courseDescription = scn.nextLine();
+					String sql4 = ("UPDATE course SET (description) VALUES(?)");
+					PreparedStatement preparedStatement4 = this.conn.prepareStatement(sql4);
+					preparedStatement4.setString(1, courseDescription);
+					break;
+					
+				case 'b': 
+					break;
+					
+				default:
+					System.out.println("You entered an unsupported command (" + u + ")");
+										
+			}
+		} while (u != 'b');
+			
+		scn.close();
+		
+	}
 }
